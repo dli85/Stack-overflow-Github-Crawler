31005015200b6ac148551641e327020a451208ea
dli85 <53060191+dli85@users.noreply.github.com>
2020-06-22 21:32:04 -0400

    Update trending_projects.py

--- trending_projects.py
+++ trending_projects.py
@@ -26,17 +26,7 @@ github_trending = []
 
 
 def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='¦'):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  :z suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
+
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
